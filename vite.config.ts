25cc9f7c50a4dfe74710a71b191131c51ba7f08a
odrii2710 <odrii2710@gmail.com>
2026-01-14 19:28:30 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,14 @@
-/// <reference types="vite/client" />
+/// <reference types="node" />
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.API_KEY),
+    },
+  };
+});
