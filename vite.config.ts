a33174c1ef0438bf5d2fe43c61346ad46197972c
odrii2710 <odrii2710@gmail.com>
2026-01-14 19:20:19 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Declare process for TypeScript to avoid "Cannot find name 'process'" error
-declare const process: any;
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import process from 'node:process';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -12,5 +10,5 @@ export default defineConfig(({ mode }) => {
     define: {
       'process.env.API_KEY': JSON.stringify(env.API_KEY),
     },
-  }
-})
+  };
+});
